7656f42ed8a7ec6b8ed64ba84fbceff323c7012d
David <30422759+davalimi@users.noreply.github.com>
2024-08-11 15:02:57 +0300

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,6 +1,10 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+docker-compose down;
+sed -i "s~DOMAIN_TO_CHANGE~${DOMAIN}~g" ./docker-compose.yml
+docker-compose up -d;
+
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 300s;
