e45d859f40b5d15c3a9a6b398a4be264eebc52f7
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-08-13 09:14:06 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ./docker/build.sh
+rm -rf ./docker/README.md
 cp -r ./docker/* ./
 
 docker buildx build . --output type=docker,name=elestio4test/documenso:latest | docker load
